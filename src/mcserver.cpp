eb87d16efa4414065c9b5bca23f7ea64b8239e37
Josh Williams <josh@cedarhouselabs.com>
2013-10-19 08:15:25 -0500

    Actually launching server! Yay!

--- src/mcserver.cpp
+++ src/mcserver.cpp
@@ -3,11 +3,42 @@
 
 #include <jni.h>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 #include <cstdlib>
+#include <unistd.h>
+#include <limits.h>
 
 #include "dynamiclib.h"
 
+#define IGNORE_RETURN(fn) if (fn)
+
 extern "C" typedef jint (*JNI_CreateJavaVM_t)(JavaVM**, void*, JavaVMInitArgs*);
+
+void getWorkingDir(std::string& pPath, const char* pProg) {
+	char vBuffer[PATH_MAX];
+	IGNORE_RETURN(getcwd(vBuffer, PATH_MAX));
+	std::string vProgramName = vBuffer;
+	vProgramName += "/";
+	vProgramName += pProg;
+	std::vector<std::string> vChunks;
+	std::string vTemp;
+	std::istringstream iss(vProgramName);
+	while (std::getline(iss, vTemp, '/')) {
+		if (vTemp.compare("..") == 0) {
+			vChunks.pop_back();
+		} else if (vTemp.compare(".") != 0 && vTemp.length() > 0) {
+			vChunks.push_back(vTemp);
+		}
+	}
+
+	vChunks.pop_back(); // Delete the actual program name
+	for (std::vector<std::string>::const_iterator vIt = vChunks.begin(); vIt != vChunks.end(); vIt++) {
+		pPath += "/";
+		pPath += *vIt;
+	}
+}
 
 MCServer::DynamicLib* loadVM() {
 	std::string jLibPath = std::getenv("JAVA_HOME");
@@ -15,10 +46,9 @@ MCServer::DynamicLib* loadVM() {
 #if MCSERVER_ARCH == ARCH_32
 	jLibPath += "i386";
 #elif MCSERVER_ARCH == ARCH_64
-	jLibPath += "x86_64";
+	jLibPath += "amd64";
 #endif
 	jLibPath += "/server/";
-	std::cout << "jLibPath: " << jLibPath << std::endl;
 	dlerror();
 
 	MCServer::DynamicLib *jLib = new MCServer::DynamicLib();
@@ -28,17 +58,22 @@ MCServer::DynamicLib* loadVM() {
 		std::cerr << "Unable to load JVM" << std::endl;
 		return NULL;
 	}
-	std::cout << "JVM Loaded" << std::endl;
 	return jLib;
 }
 
 
 int main(int argc, char *argv[]) {
-	JavaVM *jvm;
-	JNIEnv *env;
+	std::string vJarPath;
+	getWorkingDir(vJarPath, argv[0]);
+	std::cout << "Working dir: " << vJarPath << std::endl;
+	vJarPath += "/minecraft_server.1.6.2.jar";
+	std::string vClassPath = const_cast<char*>("-Djava.class.path=/usr/lib/java:");
+	vClassPath += vJarPath;
+	JavaVM *jvm = NULL;
+	JNIEnv *env = NULL;
 	JavaVMInitArgs vm_args;
 	JavaVMOption* options = new JavaVMOption[1];
-	options[0].optionString = const_cast<char*>("-Djava.class.path=/usr/lib/java");
+	options[0].optionString = const_cast<char*>(vClassPath.c_str());
 	vm_args.version = JNI_VERSION_1_6;
 	vm_args.nOptions = 1;
 	vm_args.options = options;
@@ -54,23 +89,39 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
-	std::cout << "Pointer found" << std::endl;
-
 	JNI_CreateJavaVM_t create = reinterpret_cast<JNI_CreateJavaVM_t>(reinterpret_cast<size_t>(ptr));
 	//void *_JNI_CreateJavaVM = jLib.GetSymbol("JNI_CreateJavaVM");
 
 	(*create)(&jvm, (void**)&env, &vm_args);
-	std::cout << "VM Created" << std::endl;
 	delete options;
-	jclass cls = env->FindClass("Main");
+	jclass cls = env->FindClass("net/minecraft/server/MinecraftServer");
 	if (cls == NULL) {
 		std::cerr << "Unable to find Main class" << std::endl;
 		jLib->UnLoad();
 		delete jLib;
 		return -1;
 	}
-	jmethodID mid = env->GetStaticMethodID(cls, "test", "(I)V");
-	env->CallStaticVoidMethod(cls, mid, 100);
+	jmethodID mid = env->GetStaticMethodID(cls, "main", "([Ljava/lang/String;)V");
+	if (mid == NULL) {
+		std::cerr << "Unable to find method" << std::endl;
+		jvm->DestroyJavaVM();
+		jLib->UnLoad();
+		delete jLib;
+		return -1;
+	}
+	jclass stringClass = env->FindClass("java/lang/String");
+	if (stringClass == NULL) {
+		std::cerr << "Unable to find String class" << std::endl;
+		jvm->DestroyJavaVM();
+		jLib->UnLoad();
+		delete jLib;
+		return -1;
+	}
+
+	jobjectArray vArgs = env->NewObjectArray(1, stringClass, NULL);
+	jstring vStringArg = env->NewStringUTF("--nogui");
+	env->SetObjectArrayElement(vArgs, 0, vStringArg);
+	env->CallStaticVoidMethod(cls, mid, vArgs);
 	jvm->DestroyJavaVM();
 
 	return 0;
